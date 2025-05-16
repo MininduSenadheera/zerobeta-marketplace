2b7266680959c3226b35b165e8cdc3390537e809
Minindu Senadheera <69930656+MininduSenadheera@users.noreply.github.com>
2025-05-16 20:38:55 +0530

    product card component created

---
+++ apps/web/components/ProductCard.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image"
+import { Card, CardContent } from "./ui/card"
+import { IProduct } from "@/Helpers/Interfaces"
+import { useRouter } from "next/navigation"
+
+function ProductCard(props: { product: IProduct }) {
+  const router = useRouter()
+
+  return (
+    <Card className="p-0 hover:shadow-lg transition-shadow" onClick={() => router.push(`/products/${props.product.id}`)}>
+      <Image
+        src={props.product.images[0] as string} alt={props.product.name}
+        width={400} height={300}
+        className="w-full h-60 object-cover rounded-t-xl"
+      />
+      <CardContent className="mb-6">
+        <h2 className="text-lg font-semibold">{props.product.name}</h2>
+        <p className="text-sm text-muted-foreground">{props.product.description}</p>
+        <div className="flex items-center gap-4 mt-2">
+          <p className="text-md font-medium">${props.product.price}</p>
+          <p className="text-sm text-muted-foreground">{props.product.orderCount} sold</p>
+        </div>
+      </CardContent>
+    </Card>
+  )
+}
+
+export default ProductCard
