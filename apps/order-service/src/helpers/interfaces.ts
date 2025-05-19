d6a068314441b7c83c0885dca9d4f3a9f48423bb
Minindu Senadheera <69930656+MininduSenadheera@users.noreply.github.com>
2025-05-19 22:38:50 +0530

    interfaces and types for order service

---
+++ apps/order-service/src/helpers/interfaces.ts
@@ -0,0 +1,51 @@
+import { OrderStatusTypes, ShippingMethodTypes, UserRoleTypes } from "./types"
+
+export interface IProduct {
+  id: string
+  code: string
+  name: string
+  images: string[]
+  price: number
+  description: string
+  stock: number
+  orderCount: number
+  isDeleted: boolean
+}
+
+export interface IOrder {
+  id: string
+  email: string
+  firstname: string
+  lastname: string
+  mobile: string
+  address: string
+  city: string
+  country: string
+  products: {
+    product: IProduct,
+    quantity: number
+    unitPrice: number
+  }[]
+  shippingMethod: ShippingMethodTypes
+  shippingCost: number
+  totalPrice: number
+  date: Date
+  status: OrderStatusTypes
+}
+
+export interface IUser {
+  id: string
+  email: string
+  firstname: string
+  lastname: string
+  country: string
+  userRole: UserRoleTypes
+}
+
+export interface IStockUpdatePayload {
+  type: 'increase' | 'decrease'
+  items: {
+    productId: string
+    quantity: number
+  }[]
+}
