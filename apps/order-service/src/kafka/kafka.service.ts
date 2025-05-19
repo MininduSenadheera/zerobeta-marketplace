551c7b6943cc5e9e901e0ec4a8afdb7933eb136f
Minindu Senadheera <69930656+MininduSenadheera@users.noreply.github.com>
2025-05-19 22:39:28 +0530

    kafka service for order service

---
+++ apps/order-service/src/kafka/kafka.service.ts
@@ -0,0 +1,70 @@
+import { Inject, Injectable, ServiceUnavailableException } from "@nestjs/common";
+import { ClientKafka } from "@nestjs/microservices";
+import { firstValueFrom } from "rxjs";
+import { IProduct, IStockUpdatePayload, IUser } from "src/helpers/interfaces";
+
+@Injectable()
+export class KafkaEventService {
+  constructor(
+    @Inject('PRODUCT_SERVICE') private readonly productClient: ClientKafka,
+    @Inject('USER_SERVICE') private readonly userClient: ClientKafka
+  ) { }
+
+  async onModuleInit() {
+    await this.productClient.connect();
+    await this.userClient.connect();
+  }
+
+  notifyStockAndOrderCountUpdate({ type, items }: IStockUpdatePayload) {
+    this.productClient.emit<IStockUpdatePayload>('product.stock.update', {
+      type,
+      items,
+    });
+  }
+
+  async getProductsData(productIds: string[]): Promise<IProduct[]> {
+    try {
+      if (!productIds.length) return [];
+      const { products } = await firstValueFrom<{ products: IProduct[] }>(
+        this.productClient.send('product.get.details.bulk', { productIds })
+      );
+      return products;
+    } catch {
+      throw new ServiceUnavailableException('Failed to fetch product details');
+    }
+  }
+
+  async getBuyersData(userIds: string[]): Promise<IUser[]> {
+    try {
+      if (!userIds.length) return [];
+      const { buyers } = await firstValueFrom<{ buyers: IUser[] }>(
+        this.userClient.send('user.get.bulk', { userIds })
+      );
+      return buyers;
+    } catch {
+      throw new ServiceUnavailableException('Failed to fetch buyer details');
+    }
+  }
+
+  async createTempUser(email: string, firstName: string, lastName: string): Promise<string> {
+    try {
+      const { id } = await firstValueFrom<{ id: string }>(
+        this.userClient.send('user.create.temp', { email, firstName, lastName })
+      );
+      return id;
+    } catch {
+      throw new ServiceUnavailableException('Failed to fetch user details');
+    }
+  }
+
+  async fetchProductIdsBySeller(sellerId: string): Promise<string[]> {
+    try {
+      const { productIds } = await firstValueFrom<{ productIds: string[] }>(
+        this.productClient.send('product.ids.by.seller', { sellerId })
+      );
+      return productIds;
+    } catch {
+      throw new ServiceUnavailableException('Failed to fetch products by seller');
+    }
+  }
+}
