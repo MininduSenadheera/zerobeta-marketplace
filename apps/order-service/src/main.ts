fe798ca585147028058f218f1864ef08cf01b248
Minindu Senadheera <69930656+MininduSenadheera@users.noreply.github.com>
2025-05-19 22:41:46 +0530

    added swagger for order service

---
+++ apps/order-service/src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { OrderModule } from './order.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(OrderModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Order Service API')
+    .setDescription('API docs for orders microservice')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document); 
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
