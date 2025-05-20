0c8be7cd94c67d180f9f71aba09b7f65f51f5ac2
Minindu Senadheera <69930656+MininduSenadheera@users.noreply.github.com>
2025-05-21 02:20:22 +0530

    user service email indexed

--- apps/user-service/src/entities/user.entity.ts
+++ apps/user-service/src/entities/user.entity.ts
@@ -1,23 +1,22 @@
-import { IsOptional } from 'class-validator';
 import { UserRoleTypes } from 'src/enums/user.enums';
-import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+import { Column, CreateDateColumn, Entity, Index, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @Column({ length: 64 })
+  @Column()
   firstname: string;
 
-  @Column({ length: 64 })
+  @Column()
   lastname: string;
 
   @Column({ unique: true })
-  @Column({ length: 128 })
+  @Index()
   email: string;
 
-  @Column({ length: 64, nullable: true })
+  @Column({ nullable: true })
   country?: string;
 
   @Column({nullable: true})
