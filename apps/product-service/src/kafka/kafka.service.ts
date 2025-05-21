71846395a4aa2b50b952ef7243cc8f7cfb6ef792
Minindu Senadheera <69930656+MininduSenadheera@users.noreply.github.com>
2025-05-21 17:18:42 +0530

    product service listen to kafka reply

--- apps/product-service/src/kafka/kafka.service.ts
+++ apps/product-service/src/kafka/kafka.service.ts
@@ -1,23 +1,24 @@
-import { Inject, Injectable, ServiceUnavailableException } from "@nestjs/common";
+import { Inject, Injectable, OnModuleInit, ServiceUnavailableException } from "@nestjs/common";
 import { ClientKafka } from "@nestjs/microservices";
 import { firstValueFrom } from "rxjs";
 import { IUser } from "src/helpers/interfaces";
 
 @Injectable()
-export class KafkaEventService {
+export class KafkaEventService implements OnModuleInit {
   constructor(
     @Inject('USER_SERVICE') private readonly userClient: ClientKafka
   ) { }
 
   async onModuleInit() {
+    this.userClient.subscribeToResponseOf('user.get.bulk');
     await this.userClient.connect();
   }
 
   async getSellersData(userIds: string[]): Promise<IUser[]> {
     try {
       if (!userIds.length) return [];
-      const { sellers } = await firstValueFrom<{ sellers: IUser[] }>(
-        this.userClient.send('user.get.bulk', { userIds })
+      const sellers: IUser[] = await firstValueFrom<IUser[]>(
+        this.userClient.send('user.get.bulk', userIds)
       );
       return sellers;
     } catch {
